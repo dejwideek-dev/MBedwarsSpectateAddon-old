323f3edfc6e4f6fd17c9ea9d81a85c055b60fce8
dejwideek-dev <dadamiec1310+@o2.pl>
2022-07-15 09:20:23 +0200

    Now cmd works only if bw api is ready

--- src/main/java/pl/dejwideek/mbwspectateaddon/SpectateAddon.java
+++ src/main/java/pl/dejwideek/mbwspectateaddon/SpectateAddon.java
@@ -1,8 +1,5 @@
 package pl.dejwideek.mbwspectateaddon;
 
-import de.marcely.bedwars.api.BedwarsAPI;
-import de.marcely.bedwars.api.arena.Arena;
-import de.marcely.bedwars.api.arena.ArenaStatus;
 import dev.dejvokep.boostedyaml.YamlDocument;
 import dev.dejvokep.boostedyaml.dvs.versioning.BasicVersioning;
 import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
@@ -10,11 +7,7 @@ import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
 import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
 import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -50,74 +43,11 @@ public class SpectateAddon extends JavaPlugin implements CommandExecutor {
                 ex.printStackTrace();
             }
 
-            this.getCommand("watch").setExecutor(this::onCommand);
+            this.getCommand("watch").setExecutor(new SpectateCommand(this));
         }
         else {
             this.getLogger().warning("MBedwars is not enabled!");
             Bukkit.getPluginManager().disablePlugin(this);
         }
-    }
-
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(commandSender instanceof Player) {
-            Player p = (Player) commandSender;
-
-            if(p.hasPermission(config.getString("Permission"))) {
-                if(strings.length == 0) {
-                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.Usage")));
-                }
-                if(strings.length >= 1) {
-                    Player target = Bukkit.getPlayer(strings[0]);
-
-                    if(target == p) {
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.Yourself")));
-                        return true;
-                    }
-                    if(target != null) {
-                        Arena a = BedwarsAPI.getGameAPI().getArenaByPlayer(target);
-
-                        if(a != null) {
-                            ArenaStatus status = a.getStatus();
-
-                            if(status.equals(ArenaStatus.LOBBY)) {
-                                a.addPlayer(p);
-                                p.teleport(target);
-                                p.sendMessage(ChatColor.translateAlternateColorCodes(
-                                        '&', config.getString("Messages.Teleported")
-                                                .replaceAll("%player%", target.getName())
-                                                .replaceAll("%arena%", a.getName())));
-                            }
-                            if(status.equals(ArenaStatus.RUNNING)) {
-                                a.addSpectator(p);
-                                p.teleport(target);
-                                p.sendMessage(ChatColor.translateAlternateColorCodes(
-                                        '&', config.getString("Messages.Teleported")
-                                                .replaceAll("%player%", target.getName())
-                                                .replaceAll("%arena%", a.getName())));
-                            }
-                            if(status.equals(ArenaStatus.END_LOBBY)) {
-                                p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.Already-Ending")));
-                            }
-                            return true;
-                        }
-                        if(a == null) {
-                            p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.No-Inside-Arena")));
-                        }
-                    }
-                    if(target == null) {
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.Not-Found")));
-                    }
-                    return true;
-                }
-            }
-            else {
-                p.sendMessage(ChatColor.translateAlternateColorCodes(
-                        '&', config.getString("Messages.No-Permission")
-                                .replaceAll("%permission%", config.getString("Permission"))));
-                return true;
-            }
-        }
-        return false;
     }
 }
